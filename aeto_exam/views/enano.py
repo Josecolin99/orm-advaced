7936c645156c7ee8d7cb55fc008041f5fc4018d9
Hector del Angel <128656122+hectorAJM@users.noreply.github.com>
2025-09-29 14:43:10 -0600

    enano exam

--- aeto_exam/views/enano.py
+++ aeto_exam/views/enano.py
@@ -20,7 +20,7 @@ class Caso1LibrosConEditorial(TemplateView):
 
     @staticmethod
     def libros_con_editorial():
-        libros = Libro.objects.all()
+        libros = Libro.objects.all().select_related('editorial')
         for libro in libros:
             print(libro.titulo, libro.editorial.nombre)
 
@@ -44,7 +44,7 @@ class Caso2CalificacionesConLibro(TemplateView):
 
     @staticmethod
     def calificaciones_con_libro():
-        califs = LibroCalificacion.objects.all()
+        califs = LibroCalificacion.objects.all().select_related('libro')
         for c in califs:
             print(c.estrellas, "->", c.libro.titulo)
 
@@ -68,7 +68,7 @@ class Caso3AutoresConLibros(TemplateView):
 
     @staticmethod
     def autores_con_libros():
-        autores = Autor.objects.all()
+        autores = Autor.objects.prefetch_related('book')
         for a in autores:
             print(a.name, [libro.titulo for libro in a.book.all()])
 
@@ -92,7 +92,7 @@ class Caso4ValuesEjemplo(TemplateView):
 
     @staticmethod
     def values_ejemplo():
-        datos = Libro.objects.values("isbn", "titulo", "editorial__nombre")
+        datos = Libro.objects.only("isbn", "titulo", "editorial__nombre")
         for d in datos:
             print(d)
 
@@ -116,7 +116,7 @@ class Caso5OnlyEjemplo(TemplateView):
 
     @staticmethod
     def only_ejemplo():
-        libros = Libro.objects.only("titulo")
+        libros = Libro.objects.only("titulo").select_related('editorial')
         for l in libros:
             print(l.titulo, l.editorial.nombre)
 
@@ -140,7 +140,7 @@ class Caso6DeferEjemplo(TemplateView):
 
     @staticmethod
     def defer_ejemplo():
-        libros = Libro.objects.defer("desc_corta")
+        libros = Libro.objects.only('titulo')
         for l in libros:
             print(l.titulo)
 
@@ -165,9 +165,12 @@ class Caso7AutoresLibrosEditorial(TemplateView):
     @staticmethod
     def autores_libros_editorial():
         autores = Autor.objects.all()
-        for a in autores:
-            for l in a.book.all():
-                print(a.name, l.titulo, l.editorial.nombre)
+        autor_prefetch = Prefetch('libros_autores', queryset=autores, to_attr='atributo_autor')
+        libros = Libro.objects.filter(titulo__endswith='Aurora').prefetch_related(autor_prefetch)
+        for libro in libros:
+            print(f'Libro: {libro.titulo}')
+            for autor in autores:
+                print(f'autor: {autor.name}, titulo: {libro.titulo}, editorial: {libro.editorial.nombre}')
 
         """
         PROBLEMA:
